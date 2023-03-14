b0543a49fd09bf8214bd8ba0ac19a96c6b72618d
AlexandroFioretti1 <alexandro.fioretti18@gmail.com>
2023-03-14 12:10:34 +0100

    add let(parole)+ condiction IF and Else + console Log

--- jsnack2/js/main.js
+++ jsnack2/js/main.js
@@ -1,1 +1,24 @@
-alert("funziona")
+/*  L’utente inserisce due parole in successione con due prompt.
+Il software stampa prima la parola più corta, poi la parola più lunga.*/
+
+/* 
+STRUMENTI USATI
+let
+prompt
+if\else
+console.log
+ */
+/* chiedo di inserire la prima parola */
+let primaParola = prompt("Inserisci la prima parola");
+
+/* chiedo di inserire la seconda parola */
+let secondaParola = prompt("Inserisci la tua seconda parola");
+
+/* definisco quale parola far stampare per prima dal console.log */
+if (primaParola.length < secondaParola.length) {
+  console.log(primaParola);
+  console.log(secondaParola);
+} else {
+  console.log(secondaParola);
+  console.log(primaParola);
+}

30e6e56dccafc7cd3a7a69f1e5deb08636b7a947
AlexandroFioretti1 <alexandro.fioretti18@gmail.com>
2023-03-14 12:43:37 +0100

    add ciclo for=x10number and calculation the final result

--- jsnack3/js/main.js
+++ jsnack3/js/main.js
@@ -1,1 +1,19 @@
-alert("funziona");
+/* Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti. */
+
+/* strumenti usati 
+let
+for
+parseInt
+Prompt
+console.log
+ */
+
+/* chiediamo per 10 volte all'utente di inserire un numero sommando il risultato totale */
+let somma = 0;
+for (let i = 1; i <= 10; i++) {
+  let number = parseInt(prompt(`Inserisci il ${i} numero:`));
+  somma += number;
+}
+/* mettiamo in console.log il risultato del nostro utente */
+console.log(`La somma dei numeri inseriti è: ${sum}`);

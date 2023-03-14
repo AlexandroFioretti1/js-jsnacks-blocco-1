150b14ed041bb0105c99d751171ba733b2c2d5b8
AlexandroFioretti1 <alexandro.fioretti18@gmail.com>
2023-03-14 11:43:59 +0100

    add let " num1\2" prompt + console log

--- jsnack1/js/main.js
+++ jsnack1/js/main.js
@@ -1,1 +1,29 @@
-alert("funziona");
+
+/*
+ L’utente inserisce due numeri in successione, con due prompt.
+Il software stampa il maggiore.
+ */
+
+
+/* strumenti usati
+let
+if\else
+prompt
+ */
+let num1 = prompt("Inserisci il primo numero:");
+
+// chiediamo all'utente di inserire il secondo numero
+let num2 = prompt("Inserisci il secondo numero:");
+
+// convertiamo i valori inseriti dall'utente in numeri
+num1 = Number(num1);
+num2 = Number(num2);
+
+// confrontiamo i numeri e stampiamo il risultato maggiore sulla console del desktop
+if (num1 > num2) {
+  console.log("Il numero maggiore è: " + num1);
+} else if (num2 > num1) {
+  console.log("Il numero maggiore è: " + num2);
+} else {
+  console.log("I due numeri sono uguali.");
+}
